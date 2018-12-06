8e41655cff308d779bbf90915c3aea12361ecfd4
livneros <livneros@gmail.com>
2018-12-06 13:47:18 +0200

    adding Dockerfile.

--- routes/resource.js
+++ routes/resource.js
@@ -5,13 +5,16 @@ const path = "/tmp/test";
 
 router.get('/', function(req, res, next) {
   fs.readFile(path, 'utf8', function(err, contents) {
+    if (err){
+
+    }
     res.send(contents)
   });
 });
 
 
-router.post('/', function(req, res, next) {
-  fs.writeFile(path, JSON.stringify(req.body), function (err) {
+router.post('/', async function(req, res, next) {
+  await fs.writeFile(path, JSON.stringify(req.body), function (err) {
     if (err) {
       console.log(err);
       throw err;
