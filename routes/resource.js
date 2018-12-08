35c66f3929c25810af11cd0b0236ed010cae37be
livneros <livneros@gmail.com>
2018-12-08 22:47:23 +0200

    changing the output's file location.

--- routes/resource.js
+++ routes/resource.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const path = "/home/ubuntu/argos/input";
+const os = require('os');
+const path = os.homedir() + "/input";
 
 router.get('/', function(req, res) {
   fs.readFile(path, 'utf8', function(err, contents) {
